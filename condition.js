d8699be61ee3a7136cbb507f0c37af64d50c8c11
alminnaharkeya <kalminnaharkeya2020@gmail.com>
2024-11-10 21:17:31 +0600

    first commit

---
+++ condition.js
@@ -0,0 +1,23 @@
+
+var rose = 15;
+var year = 9;
+var hobby = 30;
+
+if(rose < 20){
+    console.log("Rose is red.");
+}
+else{
+    console.log("Rose is white.")
+}
+if(year > 12){
+    console.log("Rafin is my younger brother.")
+}
+else{
+    console.log("I have two brothers.")
+}
+if(hobby = 50){
+    console.log("My hobby is travelling.")
+}
+else{
+    console.log("My name is keya.")
+}
---
+++ loop.js
@@ -0,0 +1,21 @@
+
+for(var Joya = 2; Joya <= 9; Joya++){
+    console.log("Joya is a student.");
+    console.log(Joya);
+}
+
+
+
+var numbers = [1,3,6,7,4,11,16,15,18,19,];
+for(var k = 0; k <= numbers.length; k++){
+ var element = numbers [k];
+ console.log(element);
+}
+
+
+
+
+for(var rain = 5; rain < 12; rain++ ){
+    console.log("Rain rain go away.");
+    console.log(rain);
+}
